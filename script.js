1959da7201e5f7a67ba88cab38d4310db509099c
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2025-10-05 21:03:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+  const btn = document.querySelector(`.btn:nth-child(${sounds.indexOf(sound) + 1})`);
+
+  btn.addEventListener('click', () => {
+    stopSounds();
+    const audio = new Audio(`sounds/${sound}.mp3`);
+    audio.play();
+    window.currentSound = audio; // Save reference to current sound
+  });
+});
+
+document.querySelector('.stop').addEventListener('click', () => {
+  stopSounds();
+});
+
+function stopSounds() {
+  if (window.currentSound) {
+    window.currentSound.pause();
+    window.currentSound.currentTime = 0;
+  }
+}
