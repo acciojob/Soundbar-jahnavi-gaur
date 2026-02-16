d3bc3d72f7ccea9504f18cc61a2053017dc64af6
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-16 16:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let buttons = document.querySelector('#buttons')
+let currentAudio = null
+
+buttons.forEach((button)=>{
+	button.addEventListener('click',handlePlay)
+})
+
+function handlePlay(e){
+	if (e.target.classList.contains('stop')) {
+    if (currentAudio) {
+      currentAudio.pause()
+      currentAudio.currentTime = 0
+    }
+    return
+  }
+
+  const sound = e.target.dataset.sound
+
+  if (currentAudio) {
+    currentAudio.pause()
+  }
+
+  currentAudio = new Audio(sound)
+  currentAudio.play()
+}
