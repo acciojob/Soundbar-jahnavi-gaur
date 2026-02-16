713f39a606d9f3e319d79f87de972944e0db04ea
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-16 16:50:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
 const buttonsSection = document.getElementById("buttons");
-
-let currentAudio = null;
+const audio = document.getElementById("audio");
 
 buttonsSection.addEventListener("click", function (e) {
 
     if (!e.target.classList.contains("btn")) return;
 
-    // STOP BUTTON
     if (e.target.classList.contains("stop")) {
-        if (currentAudio) {
-            currentAudio.pause();
-            currentAudio.currentTime = 0;
-        }
+        audio.pause();
+        audio.currentTime = 0;
         return;
     }
 
     const soundName = e.target.dataset.sound;
 
-    // Stop previous sound
-    if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-    }
-
-    currentAudio = new Audio(`sounds/${soundName}.mp3`);
-    currentAudio.play();
+    audio.src = `sounds/${soundName}.mp3`;
+    audio.play();
 });
